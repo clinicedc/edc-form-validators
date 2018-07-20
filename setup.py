532bc8569923f4ad12a9fd772f8c2ad55d7a4ce3
erikvw <ew2789@gmail.com>
2018-07-20 16:25:40 +0200

    setup

--- setup.py
+++ setup.py
@@ -7,12 +7,15 @@ from setuptools import find_packages
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
+with open(os.path.join(os.path.dirname(__file__), 'VERSION')) as f:
+    VERSION = f.read()
+
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='edc-form-validators',
-    version='0.1.1',
+    version=VERSION,
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
@@ -23,14 +26,15 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django modelform form validation edc',
-    #  install_requires=['pandas'],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
