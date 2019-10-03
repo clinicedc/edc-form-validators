5b6af11a366787ff8d338acbf2bbb0ad05350c99
erikvw <ew2789@gmail.com>
2019-10-02 21:07:38 -0500

    add date range validator, fix re.match in out of range..

--- edc_form_validators/form_validator.py
+++ edc_form_validators/form_validator.py
@@ -1,9 +1,10 @@
 from .applicable_field_validator import ApplicableFieldValidator
 from .base_form_validator import BaseFormValidator
+from .date_range_validator import DateRangeFieldValidator
 from .many_to_many_field_validator import ManyToManyFieldValidator
 from .other_specify_field_validator import OtherSpecifyFieldValidator
-from .required_field_validator import RequiredFieldValidator
 from .range_field_validator import RangeFieldValidator
+from .required_field_validator import RequiredFieldValidator
 
 
 class FormValidator(
@@ -12,6 +13,7 @@ class FormValidator(
     OtherSpecifyFieldValidator,
     ApplicableFieldValidator,
     RangeFieldValidator,
+    DateRangeFieldValidator,
     BaseFormValidator,
 ):
 
