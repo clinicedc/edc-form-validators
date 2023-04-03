bccafaf8eb2ff7a7de95a9c7a5240fc991d52846
erikvw <ew2789@gmail.com>
2023-04-02 22:36:20 -0500

    refactor date validator

--- edc_form_validators/date_validator.py
+++ edc_form_validators/date_validator.py
@@ -1,87 +1,162 @@
+from __future__ import annotations
+
 from datetime import date, datetime
-from typing import Optional, Union
 
 from django.conf import settings
+from edc_constants.constants import EQ, GT, GTE, LT, LTE
 from edc_utils import convert_php_dateformat
+from edc_utils.date import to_local
 
-from edc_form_validators.base_form_validator import INVALID_ERROR, BaseFormValidator
+from .base_form_validator import INVALID_ERROR, BaseFormValidator
+
+
+class DateValidatorError(Exception):
+    pass
 
 
 class DateValidator(BaseFormValidator):
     def _date_is(
         self,
         op,
-        field: Optional[str] = None,
-        reference_field: Optional[str] = None,
-        msg: Optional[str] = None,
+        field: str | None = None,
+        reference_field: str = None,
+        field_value: datetime | date | None = None,
+        msg: str | None = None,
     ):
-        operators = ["<", ">", "="]
+        operators = [LT, GT, EQ, LTE, GTE]
         if op not in operators:
             raise TypeError(f"Invalid operator. Expected on of {operators}.")
-        if self.cleaned_data.get(field) and self.cleaned_data.get(reference_field):
-            try:
-                date_value = self.cleaned_data.get(field).date()
-            except AttributeError:
-                date_value = self.cleaned_data.get(field)
-            try:
-                reference_value = self.cleaned_data.get(reference_field).date()
-            except AttributeError:
-                reference_value = self.cleaned_data.get(reference_field)
-            value = self.compare_to_reference_value(op, reference_value, date_value)
-            if value:
-                self.raise_validation_error({field: msg}, INVALID_ERROR)
+        if field and field_value:
+            raise TypeError("Expected field name or field value but not both.")
+        reference_value = self._get_as_date(reference_field or "report_datetime")
+        field_value = self._get_as_date(field or field_value)
+        if field_value and reference_value:
+            if not self._compare_date_to_reference_value(op, field_value, reference_value):
+                if field:
+                    self.raise_validation_error({field: msg}, INVALID_ERROR)
+                else:
+                    self.raise_validation_error(msg, INVALID_ERROR)
 
     @staticmethod
-    def compare_to_reference_value(
-        op: str, reference_value: Union[date, datetime], date_value: date
-    ) -> Optional[bool]:
-        value = None
-        if op == "<":
-            value = reference_value < date_value
-        elif op == ">":
-            value = reference_value > date_value
-        elif op == "=":
-            value = reference_value == date_value
+    def _compare_date_to_reference_value(
+        op: str,
+        field_value: date,
+        reference_value: date,
+    ) -> bool:
+        if op == LT:
+            value = field_value < reference_value
+        elif op == LTE:
+            value = field_value >= reference_value
+        elif op == GT:
+            value = field_value > reference_value
+        elif op == GTE:
+            value = field_value >= reference_value
+        elif op == EQ:
+            value = field_value == reference_value
+        else:
+            raise DateValidatorError(f"Unknown operator. Got {op}.")
         return value
 
-    def date_is_future(self, field=None, reference_field=None, msg=None, extra_msg=None):
+    def _get_as_date(self, key_or_value: str | date | datetime) -> date | None:
+        """Returns a date or None using either a key or value.
+
+        * If key is given, gets value from cleaned data.
+        * Ensures a datetime is localized before converting to date.
+        """
+        try:
+            value = self.cleaned_data.get(key_or_value)
+        except KeyError:
+            value = key_or_value
+        if value:
+            try:
+                value.date()
+            except AttributeError:
+                pass
+            else:
+                value = to_local(value).date()
+        return value
+
+    def date_is_future_or_raise(
+        self,
+        field=None,
+        reference_field=None,
+        field_value: datetime | date | None = None,
+        msg=None,
+        extra_msg=None,
+    ):
         """Raises if date/datetime field is future relative
         to reference_field.
         """
-        reference_field = reference_field or "report_datetime"
         msg = msg or f"Invalid. Expected a future date. {extra_msg or ''}".strip()
-        self._date_is("<", field=field, reference_field=reference_field, msg=msg)
+        self._date_is(
+            GT, field=field, reference_field=reference_field, field_value=field_value, msg=msg
+        )
 
-    def date_is_past(self, field=None, reference_field=None, msg=None, extra_msg=None):
+    def date_is_past_or_raise(
+        self,
+        field=None,
+        reference_field=None,
+        field_value: datetime | date | None = None,
+        msg=None,
+        extra_msg=None,
+    ):
         """Raises if date/datetime field is past relative
         to reference_field.
         """
-        reference_field = reference_field or "report_datetime"
         msg = msg or f"Invalid. Expected a past date. {extra_msg or ''}".strip()
-        self._date_is(">", field=field, reference_field=reference_field, msg=msg)
+        self._date_is(
+            LT, field=field, reference_field=reference_field, field_value=field_value, msg=msg
+        )
 
-    def date_is_today(self, field=None, reference_field=None, msg=None, extra_msg=None):
+    def date_is_equal_or_raise(
+        self,
+        field=None,
+        reference_field=None,
+        field_value: datetime | date | None = None,
+        msg=None,
+        extra_msg=None,
+    ):
         """Raises if date/datetime field is equal
         to reference_field.
         """
-        reference_field = reference_field or "report_datetime"
-        msg = msg or f"Invalid. Expected today. {extra_msg or ''}".strip()
-        self._date_is("=", field=field, reference_field=reference_field, msg=msg)
+        msg = msg or f"Invalid. Expected dates to match. {extra_msg or ''}".strip()
+        self._date_is(
+            EQ, field=field, reference_field=reference_field, field_value=field_value, msg=msg
+        )
 
-    def invalid_if_before_report_datetime(self, field=None, report_datetime_field=None):
+    def date_before_report_datetime_or_raise(
+        self,
+        field=None,
+        report_datetime_field=None,
+    ):
+        """Convenience method if comparing with report_datetime."""
         msg = None
+        report_datetime_field = report_datetime_field or "report_datetime"
         if self.cleaned_data.get(field) and self.cleaned_data.get(report_datetime_field):
             dte = self.cleaned_data.get(report_datetime_field).strftime(
                 convert_php_dateformat(settings.DATETIME_FORMAT)
             )
             msg = f"Invalid. Cannot be before report date/time. Got {dte}"
-        return self.date_is_past(field=field, reference_field=report_datetime_field, msg=msg)
+        return self.date_is_past_or_raise(
+            field=field,
+            reference_field=report_datetime_field,
+            msg=msg,
+        )
 
-    def invalid_if_after_report_datetime(self, field=None, report_datetime_field=None):
+    def date_after_report_datetime_or_raise(
+        self,
+        field=None,
+        report_datetime_field=None,
+    ):
+        """Convenience method if comparing with report_datetime."""
         msg = None
         if self.cleaned_data.get(field) and self.cleaned_data.get(report_datetime_field):
             dte = self.cleaned_data.get(report_datetime_field).strftime(
                 convert_php_dateformat(settings.DATETIME_FORMAT)
             )
             msg = f"Invalid. Cannot be after report date/time. Got {dte}"
-        return self.date_is_future(field=field, reference_field=report_datetime_field, msg=msg)
+        return self.date_is_future_or_raise(
+            field=field,
+            reference_field=report_datetime_field,
+            msg=msg,
+        )
--- edc_form_validators/tests/test_dates.py
+++ edc_form_validators/tests/test_dates.py
@@ -1,28 +1,135 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
 from dateutil.relativedelta import relativedelta
 from django import forms
 from django.test import TestCase
 from edc_utils import get_utcnow
 
-from ..form_validator import FormValidator
+from edc_form_validators.form_validator import FormValidator
 
 
 class TestDateFieldValidator(TestCase):
-    def test_date_is_before_report_datetime(self):
+    def test_both_given_raises(self):
+        now = datetime.now().astimezone(ZoneInfo("Africa/Gaborone"))
+        past = now - relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=past, report_datetime=now))
+        with self.assertRaises(TypeError) as cm:
+            form_validator.date_is_past_or_raise(field="my_date", field_value=past)
+        self.assertIn("Expected field name or field value but not both", str(cm.exception))
+
+    def test_one_or_none_given_ok(self):
+        form_validator = FormValidator(cleaned_data=dict(my_date=None, report_datetime=None))
+        try:
+            form_validator.date_after_report_datetime_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+        now = datetime.now().astimezone(ZoneInfo("Africa/Gaborone"))
+        form_validator = FormValidator(cleaned_data=dict(my_date=None, report_datetime=now))
+        try:
+            form_validator.date_after_report_datetime_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+        form_validator = FormValidator(cleaned_data=dict(my_date=now, report_datetime=None))
+        try:
+            form_validator.date_after_report_datetime_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+
+    def test_date_is_before_report_datetime_or_raise(self):
         now = get_utcnow()
-        before = now - relativedelta(days=1)
-        form_validator = FormValidator(cleaned_data=dict(my_date=before, report_datetime=now))
+        future = now + relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=future, report_datetime=now))
         self.assertRaises(
             forms.ValidationError,
-            form_validator.invalid_if_before_report_datetime,
+            form_validator.date_before_report_datetime_or_raise,
             field="my_date",
         )
 
-    def test_date_is_after_report_datetime(self):
+    def test_date_is_after_report_datetime_or_raise(self):
         now = get_utcnow()
-        before = now + relativedelta(days=1)
-        form_validator = FormValidator(cleaned_data=dict(my_date=before, report_datetime=now))
+        past = now - relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=past, report_datetime=now))
         self.assertRaises(
             forms.ValidationError,
-            form_validator.invalid_if_after_report_datetime,
+            form_validator.date_after_report_datetime_or_raise,
             field="my_date",
         )
+
+        future = now + relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=future, report_datetime=now))
+        try:
+            form_validator.date_after_report_datetime_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+
+    def test_date_is_future_or_raise(self):
+        now = get_utcnow()
+        past = now - relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=past, report_datetime=now))
+        self.assertRaises(
+            forms.ValidationError,
+            form_validator.date_is_future_or_raise,
+            field="my_date",
+        )
+        with self.assertRaises(forms.ValidationError) as cm:
+            form_validator.date_is_future_or_raise(field="my_date")
+        self.assertIn("Expected a future date", str(cm.exception.messages))
+
+        future = now + relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=future, report_datetime=now))
+        try:
+            form_validator.date_is_future_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+
+    def test_date_is_past_or_raise(self):
+        now = get_utcnow()
+        future = now + relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=future, report_datetime=now))
+        self.assertRaises(
+            forms.ValidationError,
+            form_validator.date_is_past_or_raise,
+            field="my_date",
+        )
+        with self.assertRaises(forms.ValidationError) as cm:
+            form_validator.date_is_past_or_raise(field="my_date")
+        self.assertIn("Expected a past date", str(cm.exception.messages))
+
+        past = now - relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=past, report_datetime=now))
+        try:
+            form_validator.date_is_past_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+
+    def test_date_is_equal_or_raise(self):
+        now = get_utcnow()
+        past = now - relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=past, report_datetime=now))
+        self.assertRaises(
+            forms.ValidationError,
+            form_validator.date_is_equal_or_raise,
+            field="my_date",
+        )
+        with self.assertRaises(forms.ValidationError) as cm:
+            form_validator.date_is_equal_or_raise(field="my_date")
+        self.assertIn("Expected dates to match", str(cm.exception.messages))
+        form_validator = FormValidator(cleaned_data=dict(my_date=now, report_datetime=now))
+        try:
+            form_validator.date_is_equal_or_raise(field="my_date")
+        except forms.ValidationError:
+            self.fail("ValidationError unexpectedly raised")
+
+    def test_date_timezone(self):
+        now = datetime.now().astimezone(ZoneInfo("Africa/Gaborone"))
+        future = now + relativedelta(days=1)
+        form_validator = FormValidator(cleaned_data=dict(my_date=future, report_datetime=now))
+        self.assertRaises(
+            forms.ValidationError,
+            form_validator.date_is_past_or_raise,
+            field="my_date",
+        )
+        with self.assertRaises(forms.ValidationError) as cm:
+            form_validator.date_is_past_or_raise(field="my_date")
+        self.assertIn("Expected a past date", str(cm.exception.messages))
