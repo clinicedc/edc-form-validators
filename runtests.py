27c647ad66ba687987e554bdb9b367374805a164
erikvw <ew2789@gmail.com>
2023-12-13 17:16:15 -0600

    test setup

--- runtests.py
+++ runtests.py
@@ -1,22 +1,17 @@
 #!/usr/bin/env python
 import logging
-import os
-import sys
-from os.path import abspath, dirname
+from pathlib import Path
 
-import django
-from django.conf import settings
-from django.test.runner import DiscoverRunner
-from edc_test_utils import DefaultTestSettings
+from edc_test_utils import DefaultTestSettings, func_main
 
 app_name = "edc_form_validators"
-base_dir = dirname(abspath(__file__))
+base_dir = Path(__file__).absolute().parent
 
-DEFAULT_SETTINGS = DefaultTestSettings(
+project_settings = DefaultTestSettings(
     calling_file=__file__,
     BASE_DIR=base_dir,
     APP_NAME=app_name,
-    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    ETC_DIR=str(base_dir / app_name / "tests" / "etc"),
     INSTALLED_APPS=[
         "django.contrib.admin",
         "django.contrib.auth",
@@ -25,10 +20,6 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "django.contrib.sessions",
         "django.contrib.sites",
         "django.contrib.staticfiles",
-        # "django_crypto_fields.apps.AppConfig",
-        # "django_revision.apps.AppConfig",
-        # "multisite",
-        # "edc_dashboard.apps.AppConfig",
         "edc_form_validators.apps.AppConfig",
         "form_validators_app.apps.AppConfig",
     ],
@@ -36,13 +27,7 @@ DEFAULT_SETTINGS = DefaultTestSettings(
 
 
 def main():
-    if not settings.configured:
-        settings.configure(**DEFAULT_SETTINGS)
-    django.setup()
-    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
-    failfast = True if [t for t in sys.argv if t == "--failfast"] else False
-    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
-    sys.exit(bool(failures))
+    func_main(project_settings, f"{app_name}.tests")
 
 
 if __name__ == "__main__":
